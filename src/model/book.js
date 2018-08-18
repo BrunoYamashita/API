0aad941ef74eafacc7f88372fe5ba8472213e498
Bruno Yamashita <bruno.yamashita@ibm.com>
2018-08-18 09:07:24 -0300

    model fix

--- src/model/book.js
+++ src/model/book.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Book = new mongoose.Schema({
     title: { type: String, required: true },
-    user: { type: String, required: true },
+    author: { type: String, required: true },
     yearPublished: { type: String, required: true },
     price: { type: String, required: true },
     rating: { type: String, required: true }
